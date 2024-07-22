e8ed276757471718c7b96afc9016630b12186763
Story <pathakharshit281@gmail.com>
2024-07-22 11:44:05 +0530

    create problem and contest page UI changes

--- components/pages/createcontest/contestform.tsx
+++ components/pages/createcontest/contestform.tsx
@@ -9,13 +9,14 @@ import { FormError } from "../../form-error";
 import { FormSuccess } from "../../form-success";
 import { useSession } from "next-auth/react";
 import { UserRole } from "@prisma/client";
+import { Button } from "@/components/ui/button";
 
 interface Problem {
   id: string;
   title: string;
 }
 
-const ContestCreate: NextPage = () => {
+const ContestCreate: NextPage = (props) => {
   const { data: session, status } = useSession({ required: true });
   const [searchQuery, setSearchQuery] = useState("");
   const [error, setError] = useState<string | undefined>("");
@@ -31,7 +32,6 @@ const ContestCreate: NextPage = () => {
   } = useForm<z.infer<typeof ContestSchema>>();
 
   useEffect(() => {
-    // Fetch problems from the database
     const fetchProblems = async () => {
       const response = await fetch("/api/problems");
       const data = await response.json();
@@ -82,61 +82,65 @@ const ContestCreate: NextPage = () => {
   );
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-5 border rounded shadow">
-      <h2 className="text-2xl font-bold mb-5">Create New Contest</h2>
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-        <div>
-          <label className="block mb-2 text-sm font-medium text-gray-700">
-            Contest Name
-          </label>
-          <input
-            disabled={isPending}
-            type="text"
-            {...register("name", { required: true })}
-            className={`block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
-              errors.name ? "border-red-500" : ""
-            }`}
-          />
-          {errors.name && (
-            <span className="text-sm text-red-500">
-              Contest name is required
-            </span>
-          )}
+    <div className="w-full max-w-6xl mx-auto mt-10 p-4 border rounded shadow bg-white h-[calc(100vh-6rem)] overflow-y-auto scrollbar-hide">
+      <h2 className="text-2xl font-bold mb-4">Create New Contest</h2>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div>
+            <label className="block mb-1 text-sm font-medium text-gray-700">
+              Contest Name
+            </label>
+            <input
+              disabled={isPending}
+              type="text"
+              {...register("name", { required: true })}
+              className={`block w-full px-3 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+                errors.name ? "border-red-500" : ""
+              }`}
+            />
+            {errors.name && (
+              <span className="text-xs text-red-500">
+                Contest name is required
+              </span>
+            )}
+          </div>
+          <div>
+            <label className="block mb-1 text-sm font-medium text-gray-700">
+              Start Date and Time
+            </label>
+            <input
+              disabled={isPending}
+              type="datetime-local"
+              {...register("startTime", { required: true })}
+              className={`block w-full px-3 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+                errors.startTime ? "border-red-500" : ""
+              }`}
+            />
+            {errors.startTime && (
+              <span className="text-xs text-red-500">
+                Start time is required
+              </span>
+            )}
+          </div>
         </div>
         <div>
-          <label className="block mb-2 text-sm font-medium text-gray-700">
-            Start Date and Time
-          </label>
-          <input
-            disabled={isPending}
-            type="datetime-local"
-            {...register("startTime", { required: true })}
-            className={`block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
-              errors.startTime ? "border-red-500" : ""
-            }`}
-          />
-          {errors.startTime && (
-            <span className="text-sm text-red-500">Start time is required</span>
-          )}
-        </div>
-        <div>
-          <label className="block mb-2 text-sm font-medium text-gray-700">
+          <label className="block mb-1 text-sm font-medium text-gray-700">
             End Date and Time
           </label>
           <input
             disabled={isPending}
             type="datetime-local"
             {...register("endTime", { required: true })}
-            className={`block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+            className={`block w-full px-3 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
               errors.endTime ? "border-red-500" : ""
             }`}
           />
           {errors.endTime && (
-            <span className="text-sm text-red-500">End time is required</span>
+            <span className="text-xs text-red-500">End time is required</span>
           )}
         </div>
         <div>
-          <label className="block mb-2 text-sm font-medium text-gray-700">
+          <label className="block mb-1 text-sm font-medium text-gray-700">
             Select Problems
           </label>
           <input
@@ -144,29 +148,34 @@ const ContestCreate: NextPage = () => {
             placeholder="Search problems..."
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
-            className="w-full px-3 py-2 mb-4 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            className="w-full px-3 py-1 mb-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
-          {filteredProblems.map((problem) => (
-            <div key={problem.id} className="flex items-center mb-2">
-              <input
-                type="checkbox"
-                id={problem.id}
-                checked={selectedProblems.includes(problem.id)}
-                onChange={() => handleProblemSelect(problem.id)}
-                className="mr-2"
-              />
-              <label htmlFor={problem.id}>{problem.title}</label>
-            </div>
-          ))}
+          <div className="max-h-48 overflow-y-auto pr-2">
+            {filteredProblems.map((problem) => (
+              <div key={problem.id} className="flex items-center mb-1">
+                <input
+                  type="checkbox"
+                  id={problem.id}
+                  checked={selectedProblems.includes(problem.id)}
+                  onChange={() => handleProblemSelect(problem.id)}
+                  className="mr-2"
+                />
+                <label htmlFor={problem.id} className="text-sm">
+                  {problem.title}
+                </label>
+              </div>
+            ))}
+          </div>
         </div>
         <FormError message={error} />
         <FormSuccess message={success} />
-        <button
+        <Button
+          variant={"default"}
           type="submit"
           className="w-full px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         >
           Create Contest
-        </button>
+        </Button>
       </form>
     </div>
   );
--- components/pages/createproblem/problemform.tsx
+++ components/pages/createproblem/problemform.tsx
@@ -9,6 +9,7 @@ import { FormError } from "../../form-error";
 import { FormSuccess } from "../../form-success";
 import { useSession } from "next-auth/react";
 import { UserRole } from "@prisma/client";
+import { Button } from "@/components/ui/button";
 
 interface TestCase {
   input: string;
@@ -117,27 +118,50 @@ const ProblemCreate: NextPage = (props) => {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-5 border rounded shadow">
-      <h2 className="text-2xl font-bold mb-5">Create New Problem</h2>
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-        <div>
-          <label className="block mb-2 text-sm font-medium text-gray-700">
-            Title
-          </label>
-          <input
-            disabled={isPending}
-            type="text"
-            {...register("title", { required: true })}
-            className={`block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
-              errors.title ? "border-red-500" : ""
-            }`}
-          />
-          {errors.title && (
-            <span className="text-sm text-red-500">Title is required</span>
-          )}
+    <div className="w-full max-w-6xl mx-auto mt-10 p-4 border rounded shadow bg-white h-[calc(100vh-6rem)] overflow-y-auto scrollbar-hide">
+      <h2 className="text-2xl font-bold mb-4">Create New Problem</h2>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div>
+            <label className="block mb-1 text-sm font-medium text-gray-700">
+              Title
+            </label>
+            <input
+              disabled={isPending}
+              type="text"
+              {...register("title", { required: true })}
+              className={`block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+                errors.title ? "border-red-500" : ""
+              }`}
+            />
+            {errors.title && (
+              <span className="text-xs text-red-500">Title is required</span>
+            )}
+          </div>
+          <div>
+            <label className="block mb-1 text-sm font-medium text-gray-700">
+              Difficulty
+            </label>
+            <select
+              disabled={isPending}
+              {...register("difficulty", { required: true })}
+              className={`block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
+                errors.difficulty ? "border-red-500" : ""
+              }`}
+            >
+              <option value="EASY">Easy</option>
+              <option value="MEDIUM">Medium</option>
+              <option value="HARD">Hard</option>
+            </select>
+            {errors.difficulty && (
+              <span className="text-xs text-red-500">
+                Difficulty is required
+              </span>
+            )}
+          </div>
         </div>
         <div>
-          <label className="block mb-2 text-sm font-medium text-gray-700">
+          <label className="block mb-1 text-sm font-medium text-gray-700">
             Description
           </label>
           <textarea
@@ -146,34 +170,16 @@ const ProblemCreate: NextPage = (props) => {
             className={`block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
               errors.description ? "border-red-500" : ""
             }`}
+            rows={3}
           />
           {errors.description && (
-            <span className="text-sm text-red-500">
+            <span className="text-xs text-red-500">
               Description is required
             </span>
           )}
         </div>
         <div>
-          <label className="block mb-2 text-sm font-medium text-gray-700">
-            Difficulty
-          </label>
-          <select
-            disabled={isPending}
-            {...register("difficulty", { required: true })}
-            className={`block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
-              errors.difficulty ? "border-red-500" : ""
-            }`}
-          >
-            <option value="EASY">Easy</option>
-            <option value="MEDIUM">Medium</option>
-            <option value="HARD">Hard</option>
-          </select>
-          {errors.difficulty && (
-            <span className="text-sm text-red-500">Difficulty is required</span>
-          )}
-        </div>
-        <div>
-          <label className="block mb-2 text-sm font-medium text-gray-700">
+          <label className="block mb-1 text-sm font-medium text-gray-700">
             Topic
           </label>
           <input
@@ -185,80 +191,85 @@ const ProblemCreate: NextPage = (props) => {
             }`}
           />
           {errors.topics && (
-            <span className="text-sm text-red-500">Topic is required</span>
+            <span className="text-xs text-red-500">Topic is required</span>
           )}
         </div>
         <div>
-          <label className="block mb-2 text-sm font-medium text-gray-700">
+          <label className="block mb-1 text-sm font-medium text-gray-700">
             Test Cases
           </label>
-          {testCases.map((testCase, index) => (
-            <div key={index} className="space-y-2">
-              <div>
-                <label className="block mb-2 text-sm font-medium text-gray-700">
-                  Input
-                </label>
-                <input
-                  disabled={isPending}
-                  type="text"
-                  name={`testCases[${index}].input`}
-                  value={testCase.input}
-                  onChange={(event) => handleInputChange(index, event)}
-                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
+          <div className="space-y-2 max-h-48 overflow-y-auto pr-2">
+            {testCases.map((testCase, index) => (
+              <div key={index} className="space-y-2 mb-3">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+                  <div>
+                    <label className="block mb-1 text-xs font-medium text-gray-700">
+                      Input
+                    </label>
+                    <input
+                      disabled={isPending}
+                      type="text"
+                      name={`testCases[${index}].input`}
+                      value={testCase.input}
+                      onChange={(event) => handleInputChange(index, event)}
+                      className="block w-full px-3 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                  <div>
+                    <label className="block mb-1 text-xs font-medium text-gray-700">
+                      Output
+                    </label>
+                    <input
+                      disabled={isPending}
+                      type="text"
+                      name={`testCases[${index}].output`}
+                      value={testCase.output}
+                      onChange={(event) => handleInputChange(index, event)}
+                      className="block w-full px-3 py-1 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    />
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <input
+                    disabled={isPending}
+                    type="checkbox"
+                    name={`testCases[${index}].isSample`}
+                    checked={testCase.isSample}
+                    onChange={(event) => handleCheckboxChange(index, event)}
+                    className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out mr-2"
+                  />
+                  <label className="text-xs font-medium text-gray-700">
+                    Is Sample Test Case
+                  </label>
+                  <button
+                    disabled={isPending}
+                    type="button"
+                    onClick={() => handleRemoveTestCase(index)}
+                    className="ml-auto text-red-500 hover:text-red-700 text-xs"
+                  >
+                    Remove
+                  </button>
+                </div>
               </div>
-              <div>
-                <label className="block mb-2 text-sm font-medium text-gray-700">
-                  Output
-                </label>
-                <input
-                  disabled={isPending}
-                  type="text"
-                  name={`testCases[${index}].output`}
-                  value={testCase.output}
-                  onChange={(event) => handleInputChange(index, event)}
-                  className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-              <div>
-                <label className="block mb-2 text-sm font-medium text-gray-700">
-                  Is Sample Test Case
-                </label>
-                <input
-                  disabled={isPending}
-                  type="checkbox"
-                  name={`testCases[${index}].isSample`}
-                  checked={testCase.isSample}
-                  onChange={(event) => handleCheckboxChange(index, event)}
-                  className="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
-                />
-              </div>
-              <button
-                disabled={isPending}
-                type="button"
-                onClick={() => handleRemoveTestCase(index)}
-                className="text-red-500 hover:text-red-700"
-              >
-                Remove Test Case
-              </button>
-            </div>
-          ))}
+            ))}
+          </div>
           <button
             type="button"
             onClick={handleAddTestCase}
-            className="text-blue-500 hover:text-blue-700"
+            className="text-blue-500 hover:text-blue-700 text-sm mt-2"
           >
             Add Test Case
           </button>
         </div>
         <FormError message={error} />
         <FormSuccess message={success} />
-        <button
+        <Button
+          variant={"default"}
           type="submit"
           className="w-full px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         >
           Submit
-        </button>
+        </Button>
       </form>
     </div>
   );
